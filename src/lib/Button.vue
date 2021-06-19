e4b0f2140df051fc845e1be83fe559afeeebd9c8
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-19 23:18:37 +0800

    基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -22,5 +22,41 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+@import "./src/style/theme.scss";
 
+$h: 32px;
+$radius: 4px;
+.zero-button {
+    box-sizing: border-box;
+    height: $h;
+    padding: 0 12px;
+    cursor: pointer;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    background: white;
+    color: $o-main-color;
+    border: 1px solid $o-border-color;
+    border-radius: $radius;
+    box-shadow: 0 1px 0 fade-out(black, 0.95);
+
+    & + & {
+        margin-left: 8px;
+    }
+
+    &:hover,
+    &:focus {
+        color: $o-type-primary;
+        border-color: $o-type-primary;
+    }
+
+    &:focus {
+        outline: none;
+    }
+
+    &::-moz-focus-inner {
+        border: 0;
+    }
+}
 </style>
