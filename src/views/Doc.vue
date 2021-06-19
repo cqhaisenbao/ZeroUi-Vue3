419e2b521516b802105cef1ad62ad4e5247f8626
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-19 20:46:49 +0800

    调整css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <Topnav/>
+    <div class="layout">
+        <Topnav class="nav"/>
         <div class="content">
             <aside v-if="menuVisible">
                 <h2>组件列表</h2>
@@ -28,42 +28,72 @@
 
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
-import {inject, Ref} from 'vue';
+import {inject, Ref} from "vue";
 
 export default {
     components: {Topnav},
     setup() {
-        const menuVisible = inject<Ref<boolean>>('menuVisible');
+        const menuVisible = inject<Ref<boolean>>("menuVisible"); // get
         return {menuVisible};
-    }
+    },
 };
 </script>
+
 <style lang="scss" scoped>
-.content {
-    height: calc(100vh - 54px);
+.layout {
     display: flex;
+    flex-direction: column;
+    height: 100vh;
 
-    aside {
-        background: lightblue;
-        width: 150px;
-        padding: 16px;
+    > .nav {
+        flex-shrink: 0;
+    }
 
-        > h2 {
-            margin-bottom: 4px;
-        }
-
-        > ol {
-            > li {
-                padding: 4px 0;
-            }
-        }
-
+    > .content {
+        flex-grow: 1;
+        padding-top: 60px;
+        padding-left: 156px;
         @media (max-width: 500px) {
-            position: fixed;
-            top: 0;
-            left: 0;
-            padding-top: 70px;
+            padding-left: 0;
         }
     }
+}
+
+.content {
+    display: flex;
+
+    > aside {
+        flex-shrink: 0;
+    }
+
+    > main {
+        flex-grow: 1;
+        padding: 16px;
+        background: lightgreen;
+    }
+}
+
+aside {
+    background: lightblue;
+    width: 150px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding: 70px 16px 16px;
+    height: 100%;
+
+    > h2 {
+        margin-bottom: 4px;
+    }
+
+    > ol {
+        > li {
+            padding: 4px 0;
+        }
+    }
+}
+
+main {
+    overflow: auto;
 }
 </style>
