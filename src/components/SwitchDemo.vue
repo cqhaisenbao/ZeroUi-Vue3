a1ce526689a51be52a870e7ad9920135dd3bb554
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-19 20:58:19 +0800

    初始化switch

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,12 +1,16 @@
 <template>
-    <div>switch</div>
+    <div>
+        <Switch/>
+    </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
+import Switch from "../lib/Switch.vue";
 
 export default defineComponent({
     name: "SwitchDemo",
+    components: {Switch},
     setup() {
         return {};
     }
