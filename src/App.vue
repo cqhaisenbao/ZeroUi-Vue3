2dc9c8a7e5967fc7f6b62eb3511dabfa7806c0f7
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-19 16:27:18 +0800

    实现aside切换

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
     <router-view/>
 </template>
 
-<script>
+<script lang="ts">
+import {provide, ref} from 'vue';
 
 export default {
     name: 'App',
-    components: {}
-}
+    setup() {
+        const menuVisible = ref(false);
+        provide('menuVisible', menuVisible);
+    }
+};
 </script>
