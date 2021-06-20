bd5f0a1741302a779bc86f1cda5f7501ecd152de
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-20 18:30:03 +0800

    动态切换tabs的内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -2,15 +2,15 @@
     <div>
         <div>Tabs示例</div>
         <h1>示例1</h1>
-        <Tabs>
-            <Tab title="导航1">内容1</Tab>
-            <Tab title="导航2">内容2</Tab>
+        <Tabs v-model:selected="currentTab">
+            <Tab title="导航1" name="first">内容1</Tab>
+            <Tab title="导航2" name="second">内容2</Tab>
         </Tabs>
     </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, ref} from 'vue';
 import Tabs from "../lib/Tabs.vue";
 import Tab from "../lib/Tab.vue";
 
@@ -18,7 +18,8 @@ export default defineComponent({
     name: "TabsDemo",
     components: {Tabs, Tab},
     setup() {
-        return {};
+        const currentTab=ref('first')
+        return {currentTab};
     }
 });
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,23 @@
 <template>
-    <div>
-        <div v-for="(title,index) in titles" :key="index">{{ title }}</div>
-        <component v-for="(c,index) in defaults" :key="index" :is="c"/>
+    <div class="zero-tabs">
+        <div class="zero-tabs-nav">
+            <div :class="{selected : t.name === selected}" @click="select(t.name)" class="zero-tabs-nav-item" v-for="t in titlesAngNames" :key="t.name">{{ t.title }}</div>
+        </div>
+        <div class="zero-tabs-content">
+            <component class="zero-tabs-content-item" :is="current" :key="selected"/>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {computed, defineComponent} from 'vue';
 import Tab from "./Tab.vue";
 
 export default defineComponent({
     name: "Tabs",
+    props: {
+        selected: String
+    },
     setup(props, context) {
         const defaults = context.slots.default();
         defaults.forEach(tag => {
@@ -18,12 +25,49 @@ export default defineComponent({
                 throw new Error('Tabs子标签必须是Tab');
             }
         });
-        const titles = defaults.map(tag => tag.props.title);
-        return {defaults, titles};
+        const titlesAngNames = defaults.map(tag => {
+            return {
+                title: tag.props.title,
+                name: tag.props.name
+            };
+        });
+        const current = computed(() => {
+            return defaults.filter(tag => tag.props.name === props.selected)[0];
+        });
+        const select = (name: string) => {
+            context.emit('update:selected', name);
+        };
+        return {defaults, titlesAngNames, current, select};
     }
 });
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@import "./src/style/theme.scss";
 
+.zero-tabs {
+    &-nav {
+        display: flex;
+        color: $o-main-color;
+        border-bottom: 1px solid $o-border-color;
+
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child {
+                margin-left: 0;
+            }
+
+            &.selected {
+                color: $o-type-primary;
+            }
+        }
+    }
+
+    &-content {
+        padding: 8px 0;
+    }
+}
 </style>
