0439027b779bb97da4aca219d4fbd678b5783d45
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-20 17:36:18 +0800

    运行时检查子组件内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,10 +4,17 @@
 
 <script lang="ts">
 import {defineComponent} from 'vue';
+import Tab from "./Tab.vue";
 
 export default defineComponent({
     name: "Tabs",
-    setup() {
+    setup(props, context) {
+        const defaults = context.slots.default();
+        defaults.forEach(tag => {
+            if (tag.type !== Tab) {
+                throw new Error('Tabs子标签必须是Tab');
+            }
+        });
         return {};
     }
 });
