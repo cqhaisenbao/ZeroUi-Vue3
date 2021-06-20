255626113789d59dd37caf1f09ed25c6633d7408
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-21 00:26:50 +0800

    美化文档1

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import './lib/zero.scss'
+import './lib/zero.scss';
 import './index.scss';
 import {createApp} from 'vue';
 import App from './App.vue';
 import {router} from "./router/router";
+import 'github-markdown-css';
 
 createApp(App).use(router).mount('#app');
