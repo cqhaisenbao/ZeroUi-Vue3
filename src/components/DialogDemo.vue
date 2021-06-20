4954c5fd994a69f8b0de517e4e64615d2087be8e
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-20 11:12:26 +0800

    创建dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,12 +1,16 @@
 <template>
-    <div>dialog</div>
+    <div>
+        <Dialog></Dialog>
+    </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
+import Dialog from "../lib/Dialog.vue";
 
 export default defineComponent({
     name: "DialogDemo",
+    components: {Dialog},
     setup() {
         return {};
     }
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="dialog-overlay"></div>
+        <div class="dialog-wrapper">
+            <header>标题</header>
+            <main>
+                <p>one</p>
+                <p>two</p>
+            </main>
+            <footer>
+                <Button>ok</Button>
+                <Button>cancel</Button>
+            </footer>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import Button from "./Button.vue";
+
+export default defineComponent({
+    name: "Dialog",
+    components: {Button},
+    setup() {
+        return {};
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
