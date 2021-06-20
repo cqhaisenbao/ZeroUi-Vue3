712320036dee4f8b49e9a8a3bb9be440e24e91d3
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-20 11:20:53 +0800

    dialog基础css

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,16 +1,21 @@
 <template>
     <div>
-        <div class="dialog-overlay"></div>
-        <div class="dialog-wrapper">
-            <header>标题</header>
-            <main>
-                <p>one</p>
-                <p>two</p>
-            </main>
-            <footer>
-                <Button>ok</Button>
-                <Button>cancel</Button>
-            </footer>
+        <div class="zero-dialog-overlay"></div>
+        <div class="zero-dialog-wrapper">
+            <div class="zero-dialog">
+                <header>
+                    标题
+                    <span class="zero-dialog-close"></span>
+                </header>
+                <main>
+                    <p>one</p>
+                    <p>two</p>
+                </main>
+                <footer>
+                    <Button level="main">ok</Button>
+                    <Button>cancel</Button>
+                </footer>
+            </div>
         </div>
     </div>
 </template>
@@ -28,6 +33,80 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@import "./src/style/theme.scss";
 
+$radius: 4px;
+
+.zero-dialog {
+    background: white;
+    border-radius: $radius;
+    box-shadow: 0 0 3px fade_out(black, 0.5);
+    min-width: 30em;
+    max-width: 90%;
+
+    &-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: fade_out(black, 0.5);
+        z-index: 10;
+    }
+
+    &-wrapper {
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 11;
+    }
+
+    > header {
+        padding: 12px 16px;
+        border-bottom: 1px solid $o-border-color;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 20px;
+    }
+
+    > main {
+        padding: 12px 16px;
+    }
+
+    > footer {
+        border-top: 1px solid $o-border-color;
+        padding: 12px 16px;
+        text-align: right;
+    }
+
+    &-close {
+        position: relative;
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        cursor: pointer;
+
+        &::before,
+        &::after {
+            content: '';
+            position: absolute;
+            height: 1px;
+            background: black;
+            width: 100%;
+            top: 50%;
+            left: 50%;
+        }
+
+        &::before {
+            transform: translate(-50%, -50%) rotate(-45deg);
+        }
+
+        &::after {
+            transform: translate(-50%, -50%) rotate(45deg);
+        }
+    }
+}
 </style>
