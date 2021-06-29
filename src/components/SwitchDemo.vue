087524db1de3341b84184091369cfc52d176d76e
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-06-29 22:40:40 +0800

    save

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,23 +1,83 @@
 <template>
     <div>
-        <Switch v-model:value="isChecked" :disabled="false"/>
+        <h1>Switch 组件示例 </h1>
+        <div class="demo">
+            <h2>常规用法</h2>
+            <div class="demo-component">
+                <Component :is="Switch1Demo"/>
+            </div>
+            <div class="demo-actions">
+                <Button>查看代码</Button>
+            </div>
+            <div class="demo-code">
+                <pre class="language-html" v-html=" Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')"/>
+            </div>
+        </div>
+        <div class="demo">
+            <h2>支持 disabled </h2>
+            <div class="demo-component">
+                <Component :is="Switch2Demo"/>
+            </div>
+            <div class="demo-actions">
+                <Button>查看代码</Button>
+            </div>
+            <div class="demo-code">
+                <pre class="language-html" v-html=" Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')"/>
+            </div>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-import {defineComponent, ref} from 'vue';
-import Switch from "../lib/Switch.vue";
+import Button from '../lib/Button.vue';
+import {ref} from 'vue';
+import Switch1Demo from './Switch1.demo.vue';
+import Switch2Demo from './Switch2.demo.vue';
+import 'prismjs';
+import 'prismjs/themes/prism-okaidia.css';
 
-export default defineComponent({
-    name: "SwitchDemo",
-    components: {Switch},
+const Prism = (window as any).Prism;
+
+export default {
+    components: {Button},
     setup() {
-        const isChecked = ref(false);
-        return {isChecked};
+        const bool = ref(false);
+        return {bool, Switch1Demo, Switch2Demo, Prism};
     }
-});
+};
 </script>
 
 <style lang="scss" scoped>
+@import "./src/style/theme.scss";
 
+.demo {
+    border: 1px solid $o-border-color;
+    margin: 16px 0 32px;
+
+    > h2 {
+        font-size: 20px;
+        padding: 8px 16px;
+        border-bottom: 1px solid $o-border-color;
+    }
+
+    &-component {
+        padding: 16px;
+    }
+
+    &-actions {
+        padding: 8px 16px;
+        border-top: 1px dashed $o-border-color;
+    }
+
+    &-code {
+        padding: 8px 16px;
+        border-top: 1px dashed $o-border-color;
+
+        > pre {
+            line-height: 1.1;
+            font-family: Consolas, 'Courier New', Courier, monospace;
+            margin: 0;
+        }
+    }
+}
 </style>
