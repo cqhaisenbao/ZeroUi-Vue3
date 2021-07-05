4c83736459c80033f0769f6b49a229d37cd85f84
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-07-05 22:03:56 +0800

    初步实现dropdown，有bug

--- src/components/demoItem/Dropdown1.demo.vue
+++ src/components/demoItem/Dropdown1.demo.vue
@@ -2,16 +2,25 @@
 常规用法
 </demo>
 <template>
-    <Dropdown/>
+    <Dropdown>
+        <span>下拉菜单<Icon name="icon-menudown"/></span>
+        <template #dropdown>
+            <span>西游记</span>
+            <span>三国演义</span>
+            <span>红楼梦</span>
+            <span>水浒传</span>
+        </template>
+    </Dropdown>
 </template>
 
 <script lang="ts">
 import Dropdown from "../../lib/Dropdown.vue";
+import Icon from "../../lib/Icon.vue";
 import {ref} from 'vue';
 
 export default {
-    name:'Dropdown1Demo',
-    components: {Dropdown},
+    name: 'Dropdown1Demo',
+    components: {Dropdown, Icon},
     setup() {
 
     }
--- src/lib/Dropdown.vue
+++ src/lib/Dropdown.vue
@@ -1,17 +1,27 @@
 <template>
-  <span>dropdown</span>
+    <div class="o-dropdown">
+        <div class="o-dropdown-link" @mouseenter="showDropdownItem" @mouseleave="hiddenDropdownItem">
+            <slot></slot>
+        </div>
+        <slot name="dropdown" v-if="false"></slot>
+        <ul v-show="dropdownItemVisible">
+            <li v-for="item in dropdowns" :key="item" class="dropdown-item">{{ item }}</li>
+        </ul>
+    </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, ref} from 'vue';
 
 export default defineComponent({
     name: "Dropdown",
-    props: {
-
-    },
+    props: {},
     setup(props, context) {
-
+        const dropdowns = context.slots.dropdown().map(item => item.children);
+        const dropdownItemVisible = ref(false);
+        const showDropdownItem = () => dropdownItemVisible.value = true;
+        const hiddenDropdownItem = () => dropdownItemVisible.value = false;
+        return {dropdowns, dropdownItemVisible, showDropdownItem, hiddenDropdownItem};
     }
 });
 </script>
@@ -19,4 +29,52 @@ export default defineComponent({
 <style lang="scss">
 @import "./src/style/theme.scss";
 
+.o-dropdown-link {
+    cursor: pointer;
+    color: #409eff;
+
+    svg {
+        margin-left: 5px;
+        fill: #409eff;
+    }
+}
+
+.o-dropdown {
+    display: inline-block;
+    position: relative;
+    color: #606266;
+    font-size: 14px;
+
+
+    ul {
+        transform-origin: center top 0;
+        z-index: 2029;
+        position: absolute;
+        top: 20px;
+        left: 0;
+        padding: 10px 0;
+        margin: 5px 0;
+        background-color: #fff;
+        border: 1px solid #ebeef5;
+        border-radius: 4px;
+        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, .1);
+
+        .dropdown-item {
+            list-style: none;
+            line-height: 36px;
+            padding: 0 20px;
+            margin: 0;
+            font-size: 14px;
+            color: #606266;
+            cursor: pointer;
+            outline: none;
+            white-space: nowrap;
+
+            &:hover {
+                background-color: #ecf5ff;
+                color: #66b1ff;
+            }
+        }
+    }
+}
 </style>
