52b92892a59eb7601f7980c97213de67abe1c597
cqhaisenbao <69225950+cqhaisenbao@users.noreply.github.com>
2021-07-07 22:26:16 +0800

    完成sticky组件

--- src/components/demoItem/Sticky1.demo.vue
+++ src/components/demoItem/Sticky1.demo.vue
@@ -2,7 +2,64 @@
 常规用法
 </demo>
 <template>
-    <Sticky></Sticky>
+    <div>
+        <Sticky :distance="20">
+            <div style="width: 100%;height: 100px;background: #3f51b5"></div>
+        </Sticky>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+    </div>
+
 </template>
 
 <script lang="ts">
--- src/lib/Sticky.vue
+++ src/lib/Sticky.vue
@@ -1,22 +1,63 @@
 <template>
-   <div>sticky</div>
+    <div class="o-sticky-wrapper">
+        <div class="o-sticky" ref="stickyRef" :class="{sticky}">
+            <slot></slot>
+        </div>
+    </div>
 </template>
+<script>
+import {onMounted, onUnmounted, ref} from "vue";
 
-<script lang="ts">
-import {defineComponent, PropType} from 'vue';
-
-export default defineComponent({
-    name: "Sticky",
+export default {
+    name: 'Sticky',
     props: {
-
+        distance: {
+            type: Number,
+            default: 0
+        }
     },
-    setup() {
-        return {};
+    setup(props) {
+        const sticky = ref(false)
+        const top = ref(0)
+        const stickyRef = ref(null)
+        const handleScroll = () => {
+            if (window.scrollY > top.value - props.distance) {
+                let {width, height, left} = stickyRef.value.getBoundingClientRect()
+                stickyRef.value.style.width = width + 'px'
+                stickyRef.value.style.height = height + 'px'
+                stickyRef.value.style.top = props.distance + 'px'
+                stickyRef.value.style.left = left + 'px'
+                sticky.value = true
+            } else {
+                sticky.value = false
+            }
+        }
+        onMounted(() => {
+            top.value = stickyRef.value.getBoundingClientRect().top
+            window.addEventListener('scroll', handleScroll)
+        })
+        onUnmounted(() => {
+            window.removeEventListener('scroll', handleScroll)
+        })
+        return {sticky, top, stickyRef}
     }
-});
+}
 </script>
 
-<style lang="scss">
-@import "./src/style/theme.scss";
+<style scoped lang="scss">
+.o-sticky-wrapper {
+    width: 100%;
 
+    .o-sticky {
+        width: 100%;
+        height: 100%;
+
+        &.sticky {
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: 100;
+        }
+    }
+}
 </style>
